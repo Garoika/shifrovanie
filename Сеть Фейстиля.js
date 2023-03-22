4a82bd36d65a554fde7666e46aeeec47811d2b99
Slav | Slavick <48631971+Garoika@users.noreply.github.com>
2023-03-23 00:02:34 +0300

    Update Сеть Фейстиля.js

--- Сеть Фейстиля.js
+++ Сеть Фейстиля.js
@@ -1,182 +1,103 @@
-const letters = {
-    "a": "00011",
-    "b": "11001",
-    "c": "01110",
-    "d": "01001",
-    "e": "00001",
-    "f": "01101",
-    "g": "11010",
-    "h": "10100",
-    "i": "00110",
-    "j": "01011",
-    "k": "01111",
-    "l": "10010",
-    "m": "11100",
-    "n": "01100",
-    "o": "11000",
-    "p": "10110",
-    "q": "10111",
-    "r": "01010",
-    "s": "00101",
-    "t": "10000",
-    "u": "00111",
-    "v": "11110",
-    "w": "10011",
-    "x": "11101",
-    "y": "10101",
-    "z": "10001"
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+string XOR(string a, string b);
+string combineAndConvert(string firstHalf, string secondHalf);
+
+unordered_map<char, string> letters = {
+    {'a', "00011"},
+    {'b', "11001"},
+    {'c', "01110"},
+    {'d', "01001"},
+    {'e', "00001"},
+    {'f', "01101"},
+    {'g', "11010"},
+    {'h', "10100"},
+    {'i', "00110"},
+    {'j', "01011"},
+    {'k', "01111"},
+    {'l', "10010"},
+    {'m', "11100"},
+    {'n', "01100"},
+    {'o', "11000"},
+    {'p', "10110"},
+    {'q', "10111"},
+    {'r', "01010"},
+    {'s', "00101"},
+    {'t', "10000"},
+    {'u', "00111"},
+    {'v', "11110"},
+    {'w', "10011"},
+    {'x', "11101"},
+    {'y', "10101"},
+    {'z', "10001"}
 };
 
-const word = "qwerty";
-const n = 1;
+int main() {
+    string word;
+    int n;
 
-if(n >= word.length * 5) {
-    console.log("Нельзя использовать такое большое число раундов для этого слова! (Не должно быть больше чем СЛОВО * 5)")
-    return;
+    cout << "Введите английское слово: ";
+    cin >> word;
+    cout << "Введите число раундов: ";
+    cin >> n;
+
+    if (n >= word.length() * 5) {
+        cout << "Нельзя использовать такое большое число раундов для этого слова! (Не должно быть больше чем СЛОВО * 5)" << endl;
+        return 0;
+    }
+
+    if (word.length() % 2 != 0 || word.length() == 0) {
+        cout << "Нельзя шифровать слова с нечетным кол-вом букв!" << endl;
+        return 0;
+    }
+
+    string letterInfo = "";
+    for (char c : word) {
+        letterInfo += letters[tolower(c)];
+    }
+
+    string firstHalf = letterInfo.substr(0, letterInfo.length() / 2);
+    string secondHalf = letterInfo.substr(letterInfo.length() / 2);
+
+    cout << "Было: " << firstHalf << " | " << secondHalf << endl;
+
+    for (int i = 1; i <= n; i++) {
+        string newNumbers = secondHalf + XOR(firstHalf, secondHalf);
+        string left = newNumbers.substr(0, newNumbers.length() / 2);
+        string right = newNumbers.substr(newNumbers.length() / 2);
+        firstHalf = left;
+        secondHalf = right;
+    }
+
+    cout << "Ответ: " << firstHalf << " | " << secondHalf << endl;
+    cout << "Ответ: " << combineAndConvert(firstHalf, secondHalf) << endl;
+
+    return 0;
 }
 
-if (word.length % 2 !== 0 || word.length === 0) {
-    console.log("Нельзя шифровать слова с нечетным кол-вом букв!");
-    return;
-}
-
-const letterInfo = word.split("").map(letter => letters[letter.toLowerCase()]);
-
-let firstHalf = letterInfo.slice(0, letterInfo.length / 2);
-let secondHalf = letterInfo.slice(letterInfo.length / 2);
-
-console.log("Было:", firstHalf.join(" ") + " | " + secondHalf.join(" "));
-
-for (let i = 1; i <= n; i++) {
-    let newNumbers = secondHalf.join("") + XOR(firstHalf.join(""), secondHalf.join(""));
-    let [left, right] = divideString(newNumbers);
-    firstHalf = left.split("");
-    secondHalf = right.split("");
-}
-
-console.log("Ответ:", firstHalf.join("") + " | " + secondHalf.join(""));
-
-function XOR(a, b) {
-    let result = "";
-    for (let i = 0; i < a.length; i++) {
-        result += a[i] === b[i] ? "0" : "1";
+string XOR(string a, string b) {
+    string result = "";
+    for (int i = 0; i < a.length(); i++) {
+        result += a[i] == b[i] ? "0" : "1";
     }
     return result;
 }
 
-function divideString(str) {
-    const halfLength = Math.ceil(str.length / 2);
-    return [str.slice(0, halfLength), str.slice(halfLength)];
+string combineAndConvert(string firstHalf, string secondHalf) {
+    string letterInfo = firstHalf + secondHalf;
+    string result = "";
+    for (int i = 0; i < letterInfo.length(); i += 5) {
+        string letterCode = letterInfo.substr(i, 5);
+        for (auto const& pair : letters) {
+            if (pair.second == letterCode) {
+                result += pair.first;
+                break;
+            }
+        }
+    }
+    return result;
 }
-
-// const letters = {
-//     "a": "00011",
-//     "b": "11001",
-//     "c": "01110",
-//     "d": "01001",
-//     "e": "00001",
-//     "f": "01101",
-//     "g": "11010",
-//     "h": "10100",
-//     "i": "00110",
-//     "j": "01011",
-//     "k": "01111",
-//     "l": "10010",
-//     "m": "11100",
-//     "n": "01100",
-//     "o": "11000",
-//     "p": "10110",
-//     "q": "10111",
-//     "r": "01010",
-//     "s": "00101",
-//     "t": "10000",
-//     "u": "00111",
-//     "v": "11110",
-//     "w": "10011",
-//     "x": "11101",
-//     "y": "10101",
-//     "z": "10001"
-// };
-
-// const word = "qwer" // Ввести слово с четным кол-вом букв
-
-// const n = 2
-// const letterInfo = []
-// let firstHalf = []
-// let secondHalf = []
-
-// if(n >= word.length * 5) {
-//     console.log("Нельзя использовать такое большое число раундов для этого слова! (Не должно быть больше чем СЛОВО * 5)")
-//     return;
-// }
-
-// if (word.length % 2 !== 0 || word.length === 0) {
-//     console.log("Нельзя шифровать слова с нечетным кол-вом слов!")
-//     return;
-// };
-
-// word.split("").map(item => {
-//     letterInfo.push(getLetter(item))
-// })
-
-// for (let i = 0; i < word.length / 2; i++) {
-//     firstHalf.push(letterInfo[i])
-// }
-
-// for (let i = word.length / 2; i < word.length; i++) {
-//     secondHalf.push(letterInfo[i])
-// }
-
-// console.log("Было:", firstHalf.join(" ") + " | " + secondHalf.join(" "))
-
-// for (let i = 1; i <= n; i++) {
-//     let newNumbers = blockFeistel(firstHalf, secondHalf, i)
-//     let divide = divideString(newNumbers)
-//     firstHalf = divide[0].split("")
-//     secondHalf = divide[1].split("")
-// }
-
-// console.log("Ответ:", firstHalf.join("") + " | " + secondHalf.join(""))
-
-// function getLetter(letter) {
-//     const letArray = letters[letter.toLowerCase()]
-//     return letArray
-// }
-
-// function blockFeistel(numberLeft, numberRight, key) {
-//     let left = "";
-//     let right = "";
-
-//     numberLeft.map(item => { left += item });
-//     numberRight.map(item => { right += item });
-
-//     let leftSplit = left.split("");
-
-//     left = moveElements(leftSplit, key).join("")
-//     left = XOR(left, right)
-
-//     return right + left
-// }
-
-// function moveElements(arr, n) {
-//     if (n >= arr.length) {
-//         return arr;
-//     }
-
-//     return arr.slice(n).concat(arr.slice(0, n));
-// }
-
-// function XOR(a, b) {
-//     let result = "";
-//     for (let i = 0; i < a.length; i++) {
-//         result += a[i] === b[i] ? "0" : "1";
-//     }
-//     return result;
-// }
-
-// function divideString(str) {
-//     const length = str.length;
-//     const halfLength = Math.ceil(length / 2);
-
-//     return [str.slice(0, halfLength), str.slice(halfLength)];
-// }
